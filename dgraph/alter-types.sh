5017e19cf4baee87765d425b0724c259d889c1f2
Pau Fernández <pau.fernandez@upc.edu>
2019-12-03 22:10:42 +0100

    Hago un script que crea los tipos en dgraph

---
+++ dgraph/alter-types.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+if [ -z $1 ]; then
+  echo "usage: $0 <file>"
+  exit 1
+fi
+curl localhost:8080/alter -XPOST -d $'
+nombre: string @index(term) .
+asignatura.codigo: int @index(int) .
+edicion: uid .
+tipo.evaluacion: [uid] .
+tipo.tarea: [uid] .
+
+type Asignatura {
+  nombre: string
+  asignatura.codigo: int
+  edicion: uid
+  tipo.evaluacion: [uid]
+  tipo.tarea: [uid]
+}
+
+anyo: int @index(int) .
+periodo: string @index(exact) .
+grupo: [uid] .
+evaluacion: [uid] .
+tarea: [uid] .
+
+type Edicion {
+  anyo: int
+  periodo: string
+  grupo: [uid]
+  evaluacion: [uid]
+  tarea: [uid]
+}
+
+peso: float .
+descripcion: string @index(fulltext) .
+evaluacion: [uid] .
+
+type TipoEvaluacion {
+  nombre: string
+  peso: float
+  descripcion: string
+  evaluacion: [uid]
+}
+
+type TipoTarea {
+  nombre: string
+  tarea: [uid]
+}
+
+grupo.codigo: string .
+profesor: uid .
+alumno: [uid] .
+sesion: [uid] .
+
+type Grupo {
+  nombre: string
+  grupo.codigo: string
+  profesor: uid
+  alumno: [uid]
+  sesion: [uid]
+}
+
+fecha: dateTime .
+
+type Evaluacion {
+  fecha: dateTime
+  nombre: string
+  peso: float
+  sesion: [uid]
+}
+
+entrega: [uid] .
+
+type Tarea {
+  nombre: string
+  fecha: dateTime
+  entrega: [uid]
+}
+
+fichero: uid .
+
+type Enunciado {
+  nombre: string
+  fichero: uid
+}
+
+apellidos: string .
+DNI: string .
+email: string .
+persona.nota: [uid] .
+
+type Persona {
+  nombre: string
+  apellidos: string
+  DNI: string
+  email: string
+  persona.nota: [uid]
+}
+
+type SesionEvaluacion {
+  fecha: dateTime
+}
+
+valor: float .
+persona: uid .
+
+type Nota {
+  valor: float
+  sesion: uid
+  persona: uid
+}
+
+type Entrega {
+  fecha: dateTime
+  nota: uid
+  fichero: [uid]
+}
+
+type Fichero {
+  nombre: string
+}
+
+' | python -m json.tool
